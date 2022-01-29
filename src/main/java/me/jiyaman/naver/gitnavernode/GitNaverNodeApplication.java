33332e5eb9d686f9a95a984c9012c0253d479287
jiyaman <jiyaman@naver.com>
2022-01-29 13:12:32 +0900

    1.1 최초 업로드 - 테스트 파일 추가

---
+++ src/main/java/me/jiyaman/naver/gitnavernode/GitNaverNodeApplication.java
@@ -0,0 +1,13 @@
+package me.jiyaman.naver.gitnavernode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitNaverNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitNaverNodeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/me/jiyaman/naver/gitnavernode/domain/GitNaver.java
@@ -0,0 +1,5 @@
+package me.jiyaman.naver.gitnavernode.domain;
+
+public class GitNaver {
+    String message;
+}
